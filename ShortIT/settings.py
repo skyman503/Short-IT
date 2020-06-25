948985658506406a4a2e60a66cf40bf19c94a104
skyman503 <46264021+skyman503@users.noreply.github.com>
2020-06-25 23:32:21 +0200

    Update settings.py

--- ShortIT/settings.py
+++ ShortIT/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$*h*4mu%k%eiu!k_t9d%*&msf^7rt^k5l4w3$a=66=w(5ajo!y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
